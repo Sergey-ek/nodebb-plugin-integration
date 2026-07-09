d0e13326dc12359974215e1b38ff8c7ea3d1a3d4
Sergey-ek <sdobromyslov@gmail.com>
2026-07-09 10:14:57 +0500

    Add NodeBB event listeners

--- library.js
+++ library.js
@@ -1,15 +1,32 @@
 "use strict";
 
-const Routes = require("./lib/routes");
+
+const Routes =
+require("./lib/routes");
+
+
+const Events =
+require("./lib/events");
+
 
 
 const Plugin = {};
 
+
+
 Plugin.init = function(params){
 
-    Routes.init(params.router);
+
+    Routes.init(
+        params.router
+    );
+
+
+    Events.init();
+
 
 };
+
 
 
 module.exports = Plugin;
